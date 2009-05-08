36ea9436bd1a39b21a2aa17fdd2127e0307bdb17
Megan Watson <megan@knowledgetree.com>
2009-05-08 09:09:33 +0200

    KTS-4280 Added a new function which accepts a file and writes the extracted text to another file. Removed unused binaries from the makefile. "The tika text extractor is extremely slow and occasionally returns strange results" Fixed.
    Committed by: Megan Watson
    Reviewed by: Paul Barrett

--- src/com/knowledgetree/textextraction/KTTextExtractorInterface.java
+++ src/com/knowledgetree/textextraction/KTTextExtractorInterface.java
@@ -2,7 +2,37 @@ package com.knowledgetree.textextraction;
 
 public class KTTextExtractorInterface {
 	
-	public java.util.Map<String,String> getText(byte[] data) {
-		return KTTextExtractor.get().ExtractText(data);
+	/**
+	 * Extracts the content from a given file and writes the plain text output to a file
+     *
+	 * @param String contentFilename The source file containing the content to be extracted
+     * @param String outputFilename The target file for saving the extracted text
+	 */
+	public int getTextFromFile(String contentFilename, String outputFilename) 
+    {
+        KTTextExtractor extractor = KTTextExtractor.get();
+        int result = -1;
+        
+        try
+        {
+            result = extractor.ExtractTextFromFile(contentFilename, outputFilename);
+        }
+        catch(Exception ex)
+        {
+            extractor.getLogger().error(ex.getMessage());
+            return -1;
+        }
+        return result;
+	}
+    
+	/**
+	 * Extracts the content from a data stream and returns the plain text in an SAX XML object
+	 * @param data The data to be extracted.
+	 * @return SAX XML
+	 */
+	public java.util.Map<String,String> getText(byte[] data) 
+    {
+        KTTextExtractor extractor = KTTextExtractor.get();
+        return extractor.ExtractText(data);
 	}
 }
